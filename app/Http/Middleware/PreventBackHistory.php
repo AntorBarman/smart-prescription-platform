47afaf1d0e41b8e372b793d50278a55c7627b6fb
Antor Barman <antorbr320@gmail.com>
2026-09-08 15:27:36 +0600

    Add activation route

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -16,9 +16,11 @@ class PreventBackHistory
         if ($request->is('dashboard*', 'admin*', 'doctor*', 'pharmacy*', 'medicines*', 'patients*', 'prescriptions*')) {
             return $response->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
                 ->header('Pragma', 'no-cache')
+                
                 ->header('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
         }
 
         return $response;
+
     }
 }
